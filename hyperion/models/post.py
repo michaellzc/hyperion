ec54944385272ef468beda9d6cab133e27412f20
zytwh2012 <zytwh2012@gmail.com>
2019-02-04 23:35:44 -0700

    v1

--- hyperion/models/post.py
+++ hyperion/models/post.py
@@ -1,8 +1,7 @@
 
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User
-
+from .user import UserProfile
 
 class Post(models.Model):
     '''
@@ -13,12 +12,44 @@ class Post(models.Model):
     '''
     class Meta:
         app_label = 'hyperion'
-    author = models.OneToOneField(
-        User,
-        on_delete=models.CASCADE,
-        related_name='posts'
-    )
     title = models.CharField(max_length=100)
+    author = models.OneToOneField(
+            UserProfile,
+            on_delete=models.CASCADE,
+            related_name='post_author')
     content = models.TextField()
     create_date = models.DateTimeField(default=timezone.now)
     last_modify_date = models.DateTimeField(default=timezone.now)
+    visibleTo = models.ManyToManyField(
+            UserProfile,
+            # need to be fixed later
+            # on_delete=models.ForeignKey('Post', on_delete=models.CASCADE),
+            related_name='visible'
+    )
+        
+    def __str__(self):
+        return super().__str__()+' post: '+str(self.author.pk)
+    
+    def visible_to_me(self):
+        self.visibleTo.add(self.author)
+
+    def visible_to_another_author(self, User):
+        self.visibleTo.add(User)
+
+    def visible_to_host_friends(self):
+        host_friends = self.author.get_friends(including='host')
+        for host_frend in host_friends:
+            self.visibleTo.add(host_frend)
+
+    def visible_to_my_friends(self):
+        friends = self.author.get_friends()
+        for friend in friends:
+            self.visibleTo.add(friend)
+
+    def visible_to_friends_of_friends(self):
+        friends_of_friends = self.author.get_friends_friends()
+        for friend_of_friends in friends_of_friends:
+            self.visibleTo.add(friend_of_friends)
+    
+    def visible_to_public(self):
+        self.visibleTo.set([])
