722b619c93eefb41c1fbb0a305e907cfabfeb978
zytwh2012 <zytwh2012@gmail.com>
2019-02-08 18:28:02 -0700

    all pylint warning cleared

--- hyperion/models/post.py
+++ hyperion/models/post.py
@@ -14,27 +14,27 @@ class Post(models.Model):
         app_label = 'hyperion'
     title = models.CharField(max_length=100)
     author = models.OneToOneField(
-            UserProfile,
-            on_delete=models.CASCADE,
-            related_name='post_author')
+        UserProfile,
+        on_delete=models.CASCADE,
+        related_name='post_author')
     content = models.TextField()
     create_date = models.DateTimeField(default=timezone.now)
     last_modify_date = models.DateTimeField(default=timezone.now)
     visibleTo = models.ManyToManyField(
-            UserProfile,
-            # need to be fixed later
-            # on_delete=models.ForeignKey('Post', on_delete=models.CASCADE),
-            related_name='visible'
+        UserProfile,
+        # need to be fixed later
+        # on_delete=models.ForeignKey('Post', on_delete=models.CASCADE),
+        related_name='visible'
     )
-        
+
     def __str__(self):
         return super().__str__()+' post: '+str(self.author.pk)
-    
+
     def visible_to_me(self):
         self.visibleTo.add(self.author)
 
-    def visible_to_another_author(self, User):
-        self.visibleTo.add(User)
+    def visible_to_another_author(self, user_profile):
+        self.visibleTo.add(user_profile)
 
     def visible_to_host_friends(self):
         host_friends = self.author.get_friends(including='host')
@@ -50,6 +50,6 @@ class Post(models.Model):
         friends_of_friends = self.author.get_friends_friends()
         for friend_of_friends in friends_of_friends:
             self.visibleTo.add(friend_of_friends)
-    
+
     def visible_to_public(self):
-        self.visibleTo.set([])
+        self.visibleTo.clear()
--- hyperion/tests/test_post.py
+++ hyperion/tests/test_post.py
@@ -1,64 +1,66 @@
-from datetime import datetime
-from django.db import models
-from django.utils import timezone
-from ..models.post import Post
-from ..models.user import UserProfile
-from ..models.user import Friend
-from django.contrib.auth.models import User
 from django.test import TestCase
-from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
+from ..models.post import Post
+from ..models.user import Friend
+# from django.core.exceptions import ValidationError
 
 
 class PostTestCase(TestCase):
 
     def setUp(self):
-        self.u1 = User.objects.create(
-                username='2haotianzhu',
-                first_name='haotian',
-                last_name='zhu')
-        self.u2 = User.objects.create(
-                username='1yutianzhang',
-                first_name='yutian',
-                last_name='zhang',
-            )
-        self.u3 = User.objects.create(
-                username='3zhili',
-                first_name='zhi',
-                last_name='li')
+        self.u_1 = User.objects.create(
+            username='2haotianzhu',
+            first_name='haotian',
+            last_name='zhu'
+        )
 
-        self.u4 = User.objects.create(
-                username='2zhili',
-                first_name='zhi',
-                last_name='li')
+        self.u_2 = User.objects.create(
+            username='1yutianzhang',
+            first_name='yutian',
+            last_name='zhang',
+        )
 
-        self.u5 = User.objects.create(
-                username='1zhili',
-                first_name='zhi',
-                last_name='li')
+        self.u_3 = User.objects.create(
+            username='3zhili',
+            first_name='zhi',
+            last_name='li'
+        )
 
-        Friend.objects.create(profile1=self.u1.profile, profile2=self.u2.profile)
-        Friend.objects.create(profile1=self.u1.profile, profile2=self.u4.profile)
-        Friend.objects.create(profile1=self.u2.profile, profile2=self.u3.profile)
+        self.u_4 = User.objects.create(
+            username='2zhili',
+            first_name='zhi',
+            last_name='li'
+        )
 
-    # def test_private_to_me(self):
-    #     p = Post.objects.create(
-    #     author=self.u1.profile,
-    #     title="u1_private",
-    #     content="test1"
-    #     )
-    #     p.visible_to_me()
-    #     self.assertEquals(list(p.visibleTo.all()), [self.u1.profile])
+        self.u_5 = User.objects.create(
+            username='1zhili',
+            first_name='zhi',
+            last_name='li'
+        )
+
+        Friend.objects.create(profile1=self.u_1.profile, profile2=self.u_2.profile)
+        Friend.objects.create(profile1=self.u_1.profile, profile2=self.u_4.profile)
+        Friend.objects.create(profile1=self.u_2.profile, profile2=self.u_3.profile)
+
+    def test_private_to_me(self):
+        post = Post.objects.create(
+            author=self.u_1.profile,
+            title="u1_private",
+            content="test1"
+        )
+        post.visible_to_me()
+        self.assertEqual(list(post.visibleTo.all()), [self.u_1.profile])
 
     def test_private_to_another_author(self):
-        User = self.u5.profile
-        p = Post.objects.create(
-        author=self.u1.profile,
-        title="u1_fof",
-        content="test3"
-        )  
-        p.visible_to_me()
-        p.visible_to_another_author(User)
-        self.assertEquals(list(p.visibleTo.all()),[self.u1.profile, self.u5.profile])
+        user_profile = self.u_5.profile
+        post = Post.objects.create(
+            author=self.u_1.profile,
+            title="u1_fof",
+            content="test3"
+        )
+        post.visible_to_me()
+        post.visible_to_another_author(user_profile)
+        self.assertEqual(list(post.visibleTo.all()),[self.u_1.profile, self.u_5.profile])
 
     # # def test_private_to_my_friends(self):
     # #     p = Post.objects.create(
@@ -70,32 +72,32 @@ class PostTestCase(TestCase):
     # #     self.assertEquals(list(p.visibleTo.all()),[self.u2])
 
     def test_private_to_friends_of_friends(self):
-        p = Post.objects.create(
-        author=self.u1.profile,
-        title="u1_fof",
-        content="test3"
+        post = Post.objects.create(
+            author=self.u_1.profile,
+            title="u1_fof",
+            content="test3"
         )
-        p.visible_to_me()
-        p.visible_to_friends_of_friends()
-        self.assertEquals(list(p.visibleTo.all()),[self.u1.profile,self.u3.profile])
+        post.visible_to_me()
+        post.visible_to_friends_of_friends()
+        self.assertEqual(list(post.visibleTo.all()), [self.u_1.profile, self.u_3.profile])
 
-    def test_private_to_host_friends(self):  
-        p = Post.objects.create(
-        author=self.u1.profile,
-        title= "u1_host_friends",
-        content="test2"
+    def test_private_to_host_friends(self):
+        post = Post.objects.create(
+            author=self.u_1.profile,
+            title="u1_host_friends",
+            content="test2"
         )
-        p.visible_to_me()
-        p.visible_to_host_friends()
-        self.assertEquals(list(p.visibleTo.all()),[self.u1.profile, self.u2.profile ,self.u4.profile])
+        post.visible_to_me()
+        post.visible_to_host_friends()
+        self.assertEqual(list(post.visibleTo.all()), \
+            [self.u_1.profile, self.u_2.profile, self.u_4.profile])
 
     def test_public(self):
-        p = Post.objects.create(
-        author=self.u1.profile,
-        title="u1_public",
-        content="test4"
-        )
-        p.visible_to_me()
-        p.visible_to_public()
-        self.assertEquals(list(p.visibleTo.all()),[]
-        )
+        post = Post.objects.create(
+            author=self.u_1.profile,
+            title="u1_public",
+            content="test4"
+            )
+        post.visible_to_me()
+        post.visible_to_public()
+        self.assertEqual(list(post.visibleTo.all()), [])
