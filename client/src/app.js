6bdec7f8a6b818f4d01db6e80dac5f2e48a03142
Michael Lin <michael.lin1@ibm.com>
2019-02-15 08:58:20 -0700

    fix homepage layout

--- client/src/app.js
+++ client/src/app.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import { Provider } from 'unstated';
 import { Router } from '@reach/router';
+import { createGlobalStyle } from 'styled-components/macro';
 import PrivateRoute from './components/private-route';
 // import ErrorBoundary from './components/error-boundary';
 import HomePage from './pages/home-page';
 import LoginPage from './pages/login-page';
 
+let GlobalStyle = createGlobalStyle`
+  .ant-layout {
+      overflow-x: hidden;
+  }
+`;
+
 class App extends Component {
   render() {
     return (
       <Provider>
+        <GlobalStyle />
         <Router>
           <PrivateRoute as={HomePage} path="/" />
           <LoginPage path="login" />
