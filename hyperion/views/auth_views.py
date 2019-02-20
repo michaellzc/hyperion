84cdb11ee187f57bf1e4ec5688b8aed12c485dca
Ray <cquptcxl@gmail.com>
2019-02-20 12:09:31 -0700

    refactor: re-organize project structure (#28)

--- hyperion/views/auth_views.py
+++ hyperion/views/auth_views.py
@@ -1,29 +1,10 @@
-from django.contrib.auth.models import User, Group
-
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.exceptions import AuthenticationFailed
-from rest_framework import viewsets
 
-from .serializers import UserSerializer, GroupSerializer
-
-
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
-
-
-class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
+from hyperion.serializers import UserSerializer
 
 
 class AuthView(APIView):
