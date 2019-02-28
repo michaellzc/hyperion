6c40bb8d056d06dc0a88763c4151dd307dec6b3d
Michael Lin <linzichunzf@hotmail.com>
2019-02-27 22:27:50 -0700

    fix: all endpoint should return json (#65)
    Several endpoints are not enforcing basic authentication.
    
    Create a new custom authentication class that can be reused everywhere.

---
+++ hyperion/authentication.py
@@ -0,0 +1,62 @@
+import base64
+import binascii
+
+from django.contrib.auth import authenticate, get_user_model
+from django.utils.translation import ugettext_lazy as _
+
+from rest_framework.authentication import BaseAuthentication, get_authorization_header
+from rest_framework import HTTP_HEADER_ENCODING, exceptions
+
+class HyperionBasicAuthentication(BaseAuthentication):
+    """
+    HTTP Basic authentication against username/password.
+    """
+    www_authenticate_realm = 'api'
+
+    def authenticate(self, request):
+        """
+        Returns a `User` if a correct username and password have been supplied
+        using HTTP Basic authentication.  Otherwise returns `None`.
+        """
+        auth = get_authorization_header(request).split()
+
+        if not auth or auth[0].lower() != b'basic':
+            return None
+
+        if len(auth) == 1:
+            msg = _('Invalid basic header. No credentials provided.')
+            raise exceptions.AuthenticationFailed(msg)
+        elif len(auth) > 2:
+            msg = _('Invalid basic header. Credentials string should not contain spaces.')
+            raise exceptions.AuthenticationFailed(msg)
+
+        try:
+            auth_parts = base64.b64decode(auth[1]).decode(HTTP_HEADER_ENCODING).partition(':')
+        except (TypeError, UnicodeDecodeError, binascii.Error):
+            msg = _('Invalid basic header. Credentials not correctly base64 encoded.')
+            raise exceptions.AuthenticationFailed(msg)
+
+        userid, password = auth_parts[0], auth_parts[2]
+        return self.authenticate_credentials(userid, password, request)
+
+    def authenticate_credentials(self, userid, password, request=None):
+        """
+        Authenticate the userid and password against username and password
+        with optional request for context.
+        """
+        credentials = {
+            get_user_model().USERNAME_FIELD: userid,
+            'password': password
+        }
+        user = authenticate(request=request, **credentials)
+
+        if user is None:
+            raise exceptions.AuthenticationFailed(_('Invalid username/password.'))
+
+        if not user.is_active:
+            raise exceptions.AuthenticationFailed(_('User inactive or deleted.'))
+
+        return (user, None)
+
+    def authenticate_header(self, request):
+        return 'Nice try'
--- hyperion/views/auth_views.py
+++ hyperion/views/auth_views.py
@@ -1,25 +1,14 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from rest_framework.exceptions import AuthenticationFailed
 
 from hyperion.serializers import UserSerializer
+from hyperion.authentication import HyperionBasicAuthentication
 
 
 class AuthView(APIView):
-    authentication_classes = (BasicAuthentication,)
+    authentication_classes = (HyperionBasicAuthentication,)
     permission_classes = (IsAuthenticated, AllowAny)
-
-    # https://stackoverflow.com/questions/18262643/adding-custom-response-headers-to-apiexception
-    # https://stackoverflow.com/questions/86105/how-can-i-suppress-the-browsers-authentication-dialog
-    def handle_exception(self, exc):
-        if isinstance(exc, AuthenticationFailed):
-            self.headers['WWW-Authenticate'] = ""
-            return Response({'detail': exc.detail},
-                            status=exc.status_code, exception=True)
-        else:
-            super(AuthView, self).handle_exception(exc)
 
     def get(self, request):
         user = UserSerializer(request.user, context={'request': request})
