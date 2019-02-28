71282a4c89815878ae3d877526637587b6890779
Michael Lin <michael.lin1@ibm.com>
2019-02-28 10:29:09 -0700

    fix: invalid post api response query name

--- hyperion/views/post_views.py
+++ hyperion/views/post_views.py
@@ -29,20 +29,21 @@ class PostViewSet(viewsets.ModelViewSet):
         post_data = body.get('post', None)
         if post_query == 'createPost' and post_data:
             post_data['visible_to'] = post_data.get('visible_to', [])
-            serializer = PostSerializer(data=post_data, context={'request': request})
+            serializer = PostSerializer(
+                data=post_data, context={'request': request})
             if serializer.is_valid():
                 serializer.save()
-                return Response(
-                    {'query': 'createPost',
-                     'success': True,
-                     'message': 'Author Post Created'
-                     })
+                return Response({
+                    'query': 'createPost',
+                    'success': True,
+                    'message': 'Author Post Created'
+                })
             else:
-                return Response(
-                    {'query': 'createPost',
-                     'success': False,
-                     'message': serializer.errors
-                     })
+                return Response({
+                    'query': 'createPost',
+                    'success': False,
+                    'message': serializer.errors
+                })
 
     @action(detail=True, methods=['GET'], name='get_auth_posts')
     def get_auth_posts(self, request):
@@ -50,11 +51,9 @@ class PostViewSet(viewsets.ModelViewSet):
         GET /author/posts
         '''
         serializer = PostSerializer(
-            self.queryset.filter(author=request.user.profile),
-            many=True
-        )
+            self.queryset.filter(author=request.user.profile), many=True)
         return Response({
-            'query': 'visiblePosts',
+            'query': 'posts',
             'count': len(serializer.data),
             'posts': serializer.data
         })
@@ -65,11 +64,7 @@ class PostViewSet(viewsets.ModelViewSet):
         '''
         response = super().list(request)
         data = response.data
-        response.data = {
-            'query': 'publicPosts',
-            'count': len(data),
-            'posts': data
-        }
+        response.data = {'query': 'posts', 'count': len(data), 'posts': data}
         return response
 
     def retrieve(self, request, pk):
