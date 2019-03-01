ea20a7f2f10147e65634b133752749283d60bfa5
Michael Lin <linzichunzf@hotmail.com>
2019-02-28 17:16:40 -0700

    fix: invalid post api response query name (#69)
    ## Fixed
    - return `query` name is not consistent with the spec
    
    ## Added
    - use `yapf` in pre-commit hook to autoformat python code
    
    ## References
    - https://github.com/abramhindle/CMPUT404-project-socialdistribution/blob/master/example-article.json#L26

--- hyperion/tests/tests_post_api.py
+++ hyperion/tests/tests_post_api.py
@@ -18,76 +18,53 @@ class PostViewTestCase(TestCase):
             username='2haotianzhu',
             first_name='haotian',
             last_name='zhu',
-            password='123456'
-        )
+            password='123456')
         self.u_2 = User.objects.create_user(
             username='hyuntian',
             first_name='yuntian',
             last_name='zhang',
-            password='123456'
-        )
-        credentials = base64.b64encode('{}:{}'.format(self.username, self.password).encode()).decode()
+            password='123456')
+        credentials = base64.b64encode('{}:{}'.format(
+            self.username, self.password).encode()).decode()
         self.client = Client(HTTP_AUTHORIZATION='Basic {}'.format(credentials))
 
     def test_get_one(self):
         Post.objects.create(
-            author=self.u_1.profile,
-            title="1",
-            content="test1"
-        )
+            author=self.u_1.profile, title="1", content="test1")
         response = self.client.get('/posts')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.data['posts']), 1)
-        self.assertEqual(
-            response.data['posts'][0]['author']['display_name'],
-            '2haotianzhu'
-        )
+        self.assertEqual(response.data['posts'][0]['author']['display_name'],
+                         '2haotianzhu')
 
     def test_get_many(self):
         Post.objects.create(
-            author=self.u_1.profile,
-            title="2",
-            content="test2"
-        )
+            author=self.u_1.profile, title="2", content="test2")
         Post.objects.create(
-            author=self.u_1.profile,
-            title="3",
-            content="test3"
-        )
+            author=self.u_1.profile, title="3", content="test3")
         response = self.client.get('/posts')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.data['posts']), 2)
 
     def test_get_post_by_id(self):
         Post.objects.create(
-            author=self.u_1.profile,
-            title="4",
-            content="test4"
-        )
+            author=self.u_1.profile, title="4", content="test4")
         the_id = Post.objects.get().pk
         path = '/posts/{}'.format(the_id)
         response = self.client.get(path)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(
-            response.data['post']['author']['display_name'],
-            '2haotianzhu'
-        )
+        self.assertEqual(response.data['post']['author']['display_name'],
+                         '2haotianzhu')
 
     def test_get_auth_posts(self):
         # get post by auth
         Post.objects.create(
-            author=self.u_1.profile,
-            title="5",
-            content="test5"
-        )
+            author=self.u_1.profile, title="5", content="test5")
         Post.objects.create(
-            author=self.u_2.profile,
-            title="6",
-            content="test6"
-        )
+            author=self.u_2.profile, title="6", content="test6")
         response = self.client.get('/author/posts')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data['query'], 'visiblePosts')
+        self.assertEqual(response.data['query'], 'posts')
         self.assertEqual(len(response.data['posts']), 1)
 
     def test_auth_post_a_post(self):
@@ -99,11 +76,10 @@ class PostViewTestCase(TestCase):
                 "content": "some post content",
             }
         }
-        response = self.client.post('/author/posts', data, content_type='application/json')
+        response = self.client.post(
+            '/author/posts', data, content_type='application/json')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data['query'], 'createPost')
         self.assertEqual(response.data['success'], True)
-        self.assertEqual(
-            Post.objects.all()[0].author.display_name,
-            '2haotianzhu'
-        )
+        self.assertEqual(Post.objects.all()[0].author.display_name,
+                         '2haotianzhu')
