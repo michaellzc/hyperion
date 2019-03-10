ae8ae7cfc4052ee14c48822b4519accd80c24fb5
Yuntian Zhang <yuntian1@cs.ualberta.ca>
2019-03-10 17:00:02 -0600

    first try

--- hyperion/models/post.py
+++ hyperion/models/post.py
@@ -63,7 +63,20 @@ class Post(models.Model):
 
     def visible_to_another_author(self, user_profile):
         self.visible_to.add(user_profile)
-
+    def post_accessible(self,post,user_profile):
+        if post.visibility == 'FRIENDS':
+            friends = user_profile.get_friends()
+            if post.author in friends:
+                return True
+        elif post.visibility == 'FOAF':
+            friends_of_friends = user_profile.get_friends_friends()
+            if post.author in friends_of_friends:
+                return True
+        elif post.visibility == 'PUBLIC':
+            return True    
+        elif post.visibility == 'PRIVATE' and user_profile in post.visible_to.all():
+            return True 
+        return False
     @staticmethod
     def visible_to_friends(user_profile):
         friends = user_profile.get_friends()
