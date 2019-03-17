89a5f82db00f92870ccb8c85f764f3bdac154f6a
zytwh2012 <zytwh2012@gmail.com>
2019-03-16 18:32:47 -0600

    Update comment_views.py

--- hyperion/views/comment_views.py
+++ hyperion/views/comment_views.py
@@ -6,7 +6,6 @@ from rest_framework.response import Response
 from rest_framework import viewsets
 from rest_framework import status
 from django.conf import settings
-from django.contrib.auth.models import User
 
 from hyperion.authentication import HyperionBasicAuthentication
 from hyperion.serializers import CommentSerializer
