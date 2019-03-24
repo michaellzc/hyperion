3d8a2915a368de2a430c867e69a41cb9f7b73501
haotianzhu <zhuhaotian19970215@icloud.com>
2019-03-24 16:08:28 -0600

    logs

--- hyperion/views/post_views.py
+++ hyperion/views/post_views.py
@@ -80,10 +80,10 @@ class PostViewSet(viewsets.ModelViewSet):
             print('foreign user,htz')
             try:
                 foreign_user_url = request.META["X-Request-User-ID"]
+                print(foreign_user_url,'foreign_user_url user222,htz')
                 # foreign user in our db, get all public posts and posts that
                 # are visible to or such posts' firends to this foreign user profile
                 foreign_user_profile = UserProfile.objects.get(url=foreign_user_url)
-                print('foreign user222,htz')
                 result = self.queryset.filter(
                     visibility="PUBLIC"
                 ) + Post.not_own_posts_visible_to_me(foreign_user_profile)
