d18a74e37c2a1b3b8d7bea24e93b7160a494581c
Michael Lin <michael.lin1@ibm.com>
2019-03-30 01:46:07 -0600

    fix ci

--- api/config.py
+++ api/config.py
@@ -6,14 +6,16 @@ import os
 
 ############# Postgres database settings #################
 
-POSTGRES_HOST = os.environ.get('CMPUT404_POSTGRES_HOST', None)
-POSTGRES_PORT = os.environ.get('CMPUT404_POSTGRES_PORT', None)
-POSTGRES_NAME = os.environ.get('CMPUT404_POSTGRES_NAME', None)
-POSTGRES_USER = os.environ.get('CMPUT404_POSTGRES_USER', None)
-POSTGRES_PASSWORD = os.environ.get('CMPUT404_POSTGRES_PASSWORD', None)
-TEST_POSTGRES_NAME = 'test_{}'.format(os.environ.get('TEST_POSTGRES_NAME', 'default'))
+POSTGRES_HOST = os.environ.get("CMPUT404_POSTGRES_HOST", None)
+POSTGRES_PORT = os.environ.get("CMPUT404_POSTGRES_PORT", None)
+POSTGRES_NAME = os.environ.get("CMPUT404_POSTGRES_NAME", None)
+POSTGRES_USER = os.environ.get("CMPUT404_POSTGRES_USER", None)
+POSTGRES_PASSWORD = os.environ.get("CMPUT404_POSTGRES_PASSWORD", None)
+TEST_POSTGRES_NAME = os.environ.get("TEST_POSTGRES_NAME", "default")
+# TEST_POSTGRES_NAME = 'test_{}'.format(os.environ.get('TEST_POSTGRES_NAME', 'default'))
 
 ############ App environment variables ###################
 
-HYPERION_HOSTNAME = os.environ.get('CMPUT404_HYPERION_HOSTNAME',
-                                   'https://cmput404-front.herokuapp.com')
+HYPERION_HOSTNAME = os.environ.get(
+    "CMPUT404_HYPERION_HOSTNAME", "https://cmput404-front.herokuapp.com"
+)
