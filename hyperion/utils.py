2f26a0aeed520761f77a8e908bf3403bf5b7bc21
Michael Lin <linzichunzf@hotmail.com>
2019-03-30 22:08:53 -0600

    fix: fail to get individual post (#171)
    Frontend failed to normalize the post id/url, result on double forward slash when
    doing string contactation
    
    Backend API /post/:id is not following spec when returnning local post

--- hyperion/utils.py
+++ hyperion/utils.py
@@ -1,7 +1,12 @@
+# pylint: disable=logging-not-lazy,invalid-name
+
+import logging
 import requests
 import humps
 
 from hyperion.models import Server
+
+logger = logging.getLogger(__name__)
 
 
 class ForeignServerHttpUtils:
@@ -10,7 +15,7 @@ class ForeignServerHttpUtils:
         foreign_url = "{}{}{}".format(
             server.endpoint, url, "/" if server.required_trailing_slash else ""
         )
-        print("[%s] HTTP/1.1 GET %s" % (__name__, foreign_url))
+        logger.info("[%s] HTTP/1.1 GET %s" % (__name__, foreign_url))
         return requests.get(
             foreign_url, auth=(server.foreign_db_username, server.foreign_db_password), **kwargs
         )
@@ -20,7 +25,7 @@ class ForeignServerHttpUtils:
         foreign_url = "{}{}{}".format(
             server.endpoint, url, "/" if server.required_trailing_slash else ""
         )
-        print("[%s] HTTP/1.1 POST %s" % (__name__, foreign_url))
+        logger.info("[%s] HTTP/1.1 POST %s" % (__name__, foreign_url))
         camalized_json = humps.camelize([json])[0]
         return requests.post(
             foreign_url,
