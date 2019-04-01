909b47195e891d90f48301f3a2d97ee521b3efdf
Lily <lilyzhili@gmail.com>
2019-03-31 19:20:51 -0600

    test2

--- client/src/pages/author-profile-page.js
+++ client/src/pages/author-profile-page.js
@@ -4,11 +4,12 @@ import { Row, Col, Tabs } from 'antd';
 import AppLayout from '../components/app-layout';
 import FriendList from '../components/friend-list';
 import ProfileCard from '../components/profile-card';
-import PostsStream from '../components/posts-stream';
+// import PostsStream from '../components/posts-stream';
 
 const TabPane = Tabs.TabPane;
 
-const AuthorProfilePage = ({ authorId, postId, ...props }) => {
+// const AuthorProfilePage = ({ authorId, postId, ...props }) => {
+const AuthorProfilePage = ({ authorId, ...props }) => {
   return (
     <AppLayout className="user-profile-page">
       <Row gutter={24} type="flex" justify="center" align="top">
@@ -23,11 +24,11 @@ const AuthorProfilePage = ({ authorId, postId, ...props }) => {
         <Col xs={20} sm={15} md={14} lg={13} xl={11} xxl={10}>
           <Tabs type="card">
             <TabPane tab="Posts" key="1">
-              <PostsStream
+              {/* <PostsStream
                 authorId={authorId}
                 postId={parseInt(postId)}
                 props={props}
-              />
+              /> */}
             </TabPane>
             <TabPane tab="Friends" key="2">
               <FriendList authorId={authorId} />
